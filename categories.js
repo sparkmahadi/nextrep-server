bbd575c32810c9f37ecc2b753b916962456e532c
sparkmahadi <mahadi.development@gmail.com>
2022-11-24 12:19:39 +0600

    api and db connection

---
+++ categories.js
@@ -0,0 +1,9 @@
+const obj = [
+    {img:'https://i.ibb.co/Xt6nvGg/hero603490de5e4d3.png'},
+    {img:'https://i.ibb.co/WG6zs3P/honda6034975b38ad6.png'},
+    {img:'https://i.ibb.co/f9Vv5Km/lifan603498f2d8b12.png'},
+    {img:'https://i.ibb.co/G9RWKJF/suzuki60349173638c3.png'},
+    {img:'https://i.ibb.co/7nZh5p8/tvs603492e2de032.png'},
+    {img:'https://i.ibb.co/T4w5Vqy/yamaha603495d9643ab.png'},
+    {img:'https://i.ibb.co/YkLtmB2/bajaj603493a3bf659.png'},
+]
--- index.js
+++ index.js
@@ -12,13 +12,18 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gp7ekja.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gp7ekja.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    const serviceCollection = client.db("nextrep").collection("categories");
+    const categoriesCollection = client.db("nextrep").collection("categories");
 
-    // APIs here
+    app.get('/categories', async(req, res)=>{
+        const query = {};
+        const result = await categoriesCollection.find(query).toArray();
+        res.send(result);
+    })
 }
 
 run().catch(err => console.log(err))
