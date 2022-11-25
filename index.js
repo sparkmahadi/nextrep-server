a5066ff5ba8e9dea36461e7885dfe8c0bde4924b
sparkmahadi <mahadi.development@gmail.com>
2022-11-25 11:59:46 +0600

    verify user implemented

--- index.js
+++ index.js
@@ -32,10 +32,8 @@ async function run() {
     // to load the products of brands
     app.get('/category/:id', async (req, res) => {
         const id = parseInt(req.params.id);
-        console.log(id);
         const query = { brandId: id };
         const products = await productsCollection.find(query).toArray();
-        // console.log(products);
         res.send(products);
     })
 
@@ -44,11 +42,20 @@ async function run() {
         const id = req.params.id;
             const query = { _id: ObjectId(id)};
             const product = req.body;
-            console.log(product);
             const option = {upsert: true};
-            const updateProduct = {
-                $set: {
-                    status: product.status
+            let updateProduct = {};
+            if(product.status){
+                updateProduct = {
+                    $set: {
+                        status: product.status
+                    }
+                }
+            }
+            if(product.advertised){
+                updateProduct= {
+                    $set: {
+                        advertised: product.advertised
+                    }
                 }
             }
             const result = await productsCollection.updateOne(query, updateProduct, option);
@@ -58,14 +65,22 @@ async function run() {
     // to post new product
     app.post('/products', async (req, res) => {
         const product = req.body;
-        console.log(product);
         const result = await productsCollection.insertOne(product);
         res.send(result);
     });
 
+    // to delete a product
+    app.delete('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: ObjectId(id)};
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+    })
+    
     // to get the advertised products
     app.get('/advertisedProducts', async(req, res)=>{
-        const query = {advertised: true, status: 'available'};
+        const query = {advertised: true, status: 'Available'};
         const products = await productsCollection.find(query).toArray();
         res.send(products);
     })
@@ -73,7 +88,6 @@ async function run() {
     // to load my products from seller account
     app.get('/myProducts', async(req, res)=>{
         const email = req.query.email;
-        console.log(email);
         const query = {sellerEmail: email};
         const products = await productsCollection.find(query).toArray();
         res.send(products);
@@ -82,7 +96,6 @@ async function run() {
     // to get the booked products
     app.get('/bookings', async (req, res) => {
         const email = req.query.email;
-        console.log(email);
         const query = { buyerEmail: email };
         const bookings = await bookingsCollection.find(query).toArray();
         res.send(bookings);
@@ -114,6 +127,21 @@ async function run() {
     app.post('/users', async (req, res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
+        res.send(result);
+    })
+
+    // to update verified status of users
+    app.put('/users/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email: email};
+        const verification = req.body;
+        const option = {upsert: true};
+        const updateUser = {
+            $set: {
+                verified: verification.verified
+            }
+        };
+        const result = await usersCollection.updateOne(query, updateUser, option);
         res.send(result);
     })
 
